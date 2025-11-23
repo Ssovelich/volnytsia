e717d63f42c4c983ac9991d30afbb06b8a15d8ce
Stanislav <ssovelich@gmail.com>
2025-11-23 14:04:56 +0200

    updater footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import styles from "./Footer.module.scss";
+import socialLinks from "./socialLinks";
 
 const Footer = () => {
   return (
@@ -10,27 +11,13 @@ const Footer = () => {
         </div>
         <h2 className={styles.title}>Ми в соціальних мережах:</h2>
         <ul className={styles.socialList}>
-          <li className={styles.socialItem}>
-            <Link
-              href="https://www.facebook.com/KnyazaVolnitsa/"
-              target="_blank"
-            >
-              <img src="/facebook.svg" alt="Facebook" />
-            </Link>
-          </li>
-          <li className={styles.socialItem}>
-            <Link href="https://instagram.com" target="_blank">
-              <img src="/instagram.svg" alt="Instagram" />
-            </Link>
-          </li>
-          <li className={styles.socialItem}>
-            <Link
-              href="https://www.youtube.com/@%D0%9A%D0%BD%D1%8F%D0%B6%D0%B0%D0%92%D0%BE%D0%BB%D1%8C%D0%BD%D0%B8%D1%86%D1%8F%D0%9D%D0%B0%D1%80%D0%BE%D0%B4%D0%BD%D0%B8%D0%B9%D0%B0%D0%BC%D0%B0%D1%82%D0%BE%D1%80%D1%81%D1%8C%D0%BA"
-              target="_blank"
-            >
-              <img src="/youtube.svg" alt="YouTube" />
-            </Link>
-          </li>
+          {socialLinks.map((item) => (
+            <li key={item.id} className={styles.socialItem}>
+              <Link href={item.href} target="_blank">
+                <img src={item.icon} alt={item.alt} />
+              </Link>
+            </li>
+          ))}
         </ul>
         <p className={styles.copy}>Княжа Вольниця, 2025. Всі права захищено.</p>
       </div>
