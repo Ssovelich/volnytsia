6613d9c178adf04565dc20324ec4b187a1dd1923
Stanislav <ssovelich@gmail.com>
2025-12-22 14:16:02 +0200

    fix banner

--- src/components/GalleryTabs/Banner/Banner.jsx
+++ src/components/GalleryTabs/Banner/Banner.jsx
@@ -6,39 +6,36 @@ import { galleryData } from "@/data/galleryData";
 import styles from "./Banner.module.scss";
 
 const Banner = () => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [isTransitioning, setIsTransitioning] = useState(false);
+  const [index, setIndex] = useState(0);
+  const images = galleryData.bannerImages;
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setIsTransitioning(true);
-      setTimeout(() => {
-        setCurrentImageIndex(
-          (prevIndex) => (prevIndex + 1) % galleryData.bannerImages.length
-        );
-        setIsTransitioning(false);
-      }, 1000);
-    }, 4000);
+      setIndex((prev) => (prev + 1) % images.length);
+    }, 5000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [images.length]);
 
   return (
     <div className={styles.bannerWrapper}>
-      <div
-        className={`${styles.imageContainer} ${
-          isTransitioning ? styles.fadeOut : styles.fadeIn
-        }`}
-      >
-        <Image
-          src={galleryData.bannerImages[currentImageIndex].src}
-          alt={galleryData.bannerImages[currentImageIndex].alt}
-          width={1200}
-          height={400}
-          className={styles.bannerImage}
-          layout="responsive"
-        />
-      </div>
+      {images.map((img, i) => (
+        <div
+          key={img.src}
+          className={`${styles.imageContainer} ${
+            i === index ? styles.active : styles.inactive
+          }`}
+        >
+          <Image
+            src={img.src}
+            alt={img.alt}
+            fill
+            priority={i === 0}
+            className={styles.bannerImage}
+            sizes="100vw"
+          />
+        </div>
+      ))}
     </div>
   );
 };
