310a2311974425eb9c8221bf6c80e7d444b94b9b
Stanislav <ssovelich@gmail.com>
2026-01-04 15:14:31 +0200

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
         <h2 className={styles.title}>Ми в соціальних мережах:</h2>
         <ul className={styles.socialList}>
           {socials.map((item) => (
-            <li key={item.id} className={styles.socialItem}>
+            <li key={item._id} className={styles.socialItem}>
               <Link href={item.href} target="_blank">
                 <img src={item.icon} alt={item.alt} />
               </Link>
